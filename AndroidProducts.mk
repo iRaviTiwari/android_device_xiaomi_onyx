8f4319ae5dcd58bb515af95509b65eb024c8f880
Ravi <61373598+iRaviTiwari@users.noreply.github.com>
2025-10-15 18:45:33 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,11 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_onyx.mk:lineage_onyx
+
+COMMON_LUNCH_CHOICES := \
+    lineage_onyx-ap2a-user \
+    lineage_onyx-ap2a-userdebug \
+    lineage_onyx-ap2a-eng
+
 #PRODUCT_MAKEFILES := \
 #    $(LOCAL_DIR)/aosp_onyx.mk
 
@@ -6,10 +14,3 @@
 #    aosp_onyx-trunk_staging-userdebug \
 #    aosp_onyx-trunk_staging-eng
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_onyx.mk
-
-COMMON_LUNCH_CHOICES := \
-    lineage_onyx-user \
-    lineage_onyx-userdebug \
-    lineage_onyx-eng
