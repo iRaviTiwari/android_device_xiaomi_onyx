0e2050b3d69ee8a3b3dd7f33bc468f54b62b2923
Ravi <apptester20041996@gmail.com>
2025-10-15 18:20:59 +0530

    onyx: Add LineageOS product definition and lunch combos
    Change-Id: I05939c0806f3417a2016f44ac7c5e78d97f752ee

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,15 @@
+#PRODUCT_MAKEFILES := \
+#    $(LOCAL_DIR)/aosp_onyx.mk
+
+#COMMON_LUNCH_CHOICES := \
+#    aosp_onyx-trunk_staging-user \
+#    aosp_onyx-trunk_staging-userdebug \
+#    aosp_onyx-trunk_staging-eng
+
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_onyx.mk
+    $(LOCAL_DIR)/lineage_onyx.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_onyx-trunk_staging-user \
-    aosp_onyx-trunk_staging-userdebug \
-    aosp_onyx-trunk_staging-eng
+    lineage_onyx-user \
+    lineage_onyx-userdebug \
+    lineage_onyx-eng
---
+++ lineage_onyx.mk
@@ -0,0 +1,8 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, device/xiaomi/onyx/device.mk)
+
+PRODUCT_NAME := lineage_onyx
+PRODUCT_DEVICE := onyx
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi Turbo 4 Pro
+PRODUCT_MANUFACTURER := Xiaomi
