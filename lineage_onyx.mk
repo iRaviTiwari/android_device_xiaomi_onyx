c34d8dc8b5e772982a39816321f7028a71a9f543
Ravi <61373598+iRaviTiwari@users.noreply.github.com>
2025-10-15 19:31:06 +0530

    Update lineage_onyx.mk

--- lineage_onyx.mk
+++ lineage_onyx.mk
@@ -1,8 +1,16 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+# Inherit common LineageOS configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit device-specific configuration
 $(call inherit-product, device/xiaomi/onyx/device.mk)
 
+# Inherit vendor blobs
+$(call inherit-product, vendor/xiaomi/onyx/vendor.mk)
+
+# Device identifiers
 PRODUCT_NAME := lineage_onyx
 PRODUCT_DEVICE := onyx
-PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Redmi Turbo 4 Pro
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := POCO F7
 PRODUCT_MANUFACTURER := Xiaomi
